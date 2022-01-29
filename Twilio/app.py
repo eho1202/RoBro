8b8d672b6c761c736984c8306cf5dc0d13433a8b
Alec <52588950+Linja82@users.noreply.github.com>
2022-01-29 18:45:22 -0500

    Created date_time and fixed app

--- Twilio/app.py
+++ Twilio/app.py
@@ -1,21 +1,23 @@
-from flask import Flask, request
+﻿from flask import Flask, request
 from twilio import twiml
-import os
-from twilio.rest import Client
-
-#account_sid = os.environ['TWILIO_ACCOUNT_SID']
-#auth_token = os.environ['TWILIO_AUTH_TOKEN']
-#client = Client(account_sid, auth_token)
+from date_time import get_now
 
 app = Flask(__name__)
 @app.route('/sms', methods=['POST'])
-
-
 def sms():
     number = request.form['From']
     message_body = request.form['Body']
-    print("You have a message from: " + number)
+    print("You have a message from: " + str(number))
     print(message_body)
+    log(number, message_body)
+
+    return str('Your message has been relayed.')
+
+
+def log(phoneNum, message):
+    with open('Twilio/Message Log.text', 'a') as logFile:
+        logFile.write(get_now() + " -> " + str(phoneNum) + ": " + message + "\n")
+        logFile.close() 
 
 
 if __name__ == '__main__':
