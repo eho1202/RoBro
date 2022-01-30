31e476cd7987b8c192ab09f0ee480b75d8ac1b4d
Alec <52588950+Linja82@users.noreply.github.com>
2022-01-30 03:58:11 -0500

    added text to speech

--- AssemblyAIRealTime/AssemblyAIRealTime/scripts/magicbox.py
+++ AssemblyAIRealTime/AssemblyAIRealTime/scripts/magicbox.py
@@ -1,11 +1,12 @@
 import random
 import csv
+from text_to_speech import convert_text_to_speech
 
 def selmagicbox():
     with open('magicbox.csv', encoding='utf-8-sig') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         endval = random.randint(0,19)
         for row in csv_reader:
-            print(row[endval])
+            convert_text_to_speech(str(row[endval]))
 
 #selmagicbox()
