cd16bf52ca47163318671a7179a3c47b6513db19
Alec <52588950+Linja82@users.noreply.github.com>
2022-01-30 07:51:23 -0500

    updated TTS

--- ProfanityBot/profanity_bot.py
+++ ProfanityBot/profanity_bot.py
@@ -1,5 +1,5 @@
 import requests
-from text_to_speech import convert_text_to_speech
+from text_to_speech_v2 import convert_text_to_speech
 
 
 def check_profane(text_to_check):
@@ -8,5 +8,6 @@ def check_profane(text_to_check):
     print(response.text)
     if response.text == "true":
         convert_text_to_speech("Watch your language.")
+        #Add line here for vlc playing
 
 #check_profane("What are you doing?")
